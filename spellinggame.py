42e4c5c94a32703d8e5e4a95291806606c9499fb
prechy123 <bamideleprecious100@gmail.com>
2023-10-11 14:18:01 +0100

    initial commit

---
+++ spellinggame.py
@@ -0,0 +1,33 @@
+from tabulate import tabulate
+
+
+def main():
+    level: int = get_user_level()
+    
+
+
+def get_user_level() -> int:
+    """
+    get user level
+    """
+    headers = ["Level", "Difficulty"]
+    table = [
+        [1, "Beginner"],
+        [2, "Intermediate"],
+        [3, "Advanced"],
+        [4, "Expert"],
+        [5, "Impossible"],
+        ]
+    print(tabulate(table, headers, tablefmt="simple_grid"))
+    while True:
+        level = input("Enter level[1-5]: ")
+        match level:
+            case "1" | "2" | "3" | "4" | "5":
+                return int(level)
+            case _:
+                print("level must be in range of 1 to 5")
+                continue
+    
+
+if __name__ == "__main__":
+    main()
